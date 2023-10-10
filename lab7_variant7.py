61cc303b2031a3e959cb83498e42c4e1735371a9
BoMa0103 <bengemingg@gmail.com>
2023-10-10 22:13:17 +0300

    Lab 7

---
+++ lab7_variant7.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+x = np.random.rand(3100)
+y = 2 * x + 1 + np.random.rand(3100)
+
+plt.scatter(x, y, label='Дані')
+plt.xlabel('Ознака x')
+plt.ylabel('Ознака y')
+plt.legend()
+plt.title('Дані і лінійна регресія')
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+
+print(f"Модель лінійної регресії: y = {slope:.2f}x + {intercept:.2f}")
+print(f"Коефіцієнт кореляції (r-value): {r_value:.2f}")
+print(f"P-значення: {p_value:.2f}")
+
+line = slope * x + intercept
+plt.plot(x, line, color='red', label='Лінія регресії')
+plt.legend()
+
+plt.show()
